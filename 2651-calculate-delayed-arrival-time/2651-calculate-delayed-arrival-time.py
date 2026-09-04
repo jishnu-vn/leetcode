7e52f210551df112cb5d9a31726df50ab17dc410
jishnu-vn <jishnuravi66@gmail.com>
2026-09-04 19:39:52 +0530

    Time: 0 ms (100%), Space: 12.4 MB (54.52%) - LeetHub

--- 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.py
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def findDelayedArrivalTime(self, arrivalTime, delayedTime):
         c=arrivalTime+delayedTime
-        if c==24:
-            return 0
-        if c<=24:
-            return c
-        else:
-            return abs(24-c)
+        return c%24
    
