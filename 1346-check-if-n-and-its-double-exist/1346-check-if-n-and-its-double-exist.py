4c377ca56486d2be92d0664c3fed625d44aee6d1
jishnu-vn <jishnuravi66@gmail.com>
2026-09-23 02:54:42 +0530

    Time: 11 ms (38.55%), Space: 12.6 MB (6.77%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def checkIfExist(self, arr):
+        for i in range(len(arr)):
+            for j in range(i+1,len(arr)):
+                if arr[i] == 2*arr[j] or arr[j]==arr[i]*2:
+                    return True
+        return False
+
+        
